1c351af36ba4e2a94e6f898adb15590d800ce07c
miiwo <chellewsa@gmail.com>
2021-02-09 16:30:59 -0500

    update view for api

--- front-end/src/components/weapons.jsx
+++ front-end/src/components/weapons.jsx
@@ -4,9 +4,11 @@ import SearchBar from './searchbar.jsx';
 function WeaponDetails(props) {
     return (
         <>
-            <p>{props.weapon.name}</p>
-            <p>This is a description.</p>
-            <a href='#' className='thumbnail'><img src='...' alt='...' /></a>
+            <label htmlFor='wep_name' className='font-weight-bold user-select-none'>Name:</label>
+            <p id='wep_name' className='pb-3'>{props.weapon.name}</p>
+            <label htmlFor='wep_des' className='font-weight-bold user-select-none'>Description:</label>
+            <p id='wep_des'>This is a description.</p>
+            <a href='http://placehold.it/' target='_blank' className='thumbnail'><img src='http://placehold.it/200' alt='...' className='img-fluid img-thumbnail' /></a>
         </>
     )
 }
@@ -15,7 +17,20 @@ class WeaponsPage extends Component {
 
     constructor(props) {
         super(props);
-        this.state = {weapons: [{name: 'Kappa Shoot'}, {name: 'Flower Parasol'} ]};
+        this.state = {weapons: [
+            {name: 'Kappa Shoot'},
+            {name: 'Flower Parasol'},
+            {name: 'Kappa Shoot'},
+            {name: 'Trappa Shoot'},
+            {name: 'Kappa Shoot'},
+            {name: 'Kappa Shoot'},
+            {name: 'Fappa Shoot'},
+            {name: 'Kappa Shoot'},
+            {name: 'Kappa Shoot'},
+            {name: 'Kappa Shoot'},
+            {name: 'Kappa Shoot'},
+            {name: 'Kappa Shoot'},
+        ]};
         this.onSearchClick=this.onSearchClick.bind(this);
     }
 
@@ -33,17 +48,19 @@ class WeaponsPage extends Component {
 
     render () {
         return (
-            <div>
-                <div className='page-header'><h1>Weapons</h1></div>
-                <SearchBar action={this.onSearchClick} />
-                <div className='d-inline-flex'>
+            <>
+                <div className='sticky-top bg-dark pl-3'>
+                    <h1>Weapons</h1>
+                    <SearchBar action={this.onSearchClick} />
+                </div>
+                <section className='container-fluid'>
                     {this.state.weapons.map((weapon, i) =>
-                        <div key={i} className='container rounded border border-light m-2'>
+                        <div key={i} className='rounded float-left border border-light m-2 pt-1 pb-3 pl-3 pr-3'>
                             <WeaponDetails weapon={weapon} />
                         </div>
                     )}
-                </div>
-            </div>
+                </section>
+            </>
         )
     }
 }
