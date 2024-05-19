3ffd2507e5c6da354b787d072ba6618097e8ea98
koipoi <koipond575@gmail.com>
2024-05-19 19:23:49 -0400

    Convert to .env file for db config

--- backend/models/setup.go
+++ backend/models/setup.go
@@ -5,11 +5,13 @@ import (
 	"gorm.io/gorm/logger"
 	"gorm.io/gorm"
 	"fmt"
+	"os"
 )
 var DB *gorm.DB
 
 func ConnectToMariaDB() (*gorm.DB, error) {
-	dsn := "root:password@tcp(localhost:3306)/weapons_and_charas?charset=utf8&parseTime=True&loc=Local"
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?charset=utf8&parseTime=True&loc=Local", os.Getenv("DB_USER"), os.Getenv("DB_PASS"), os.Getenv("DB_HOST"), os.Getenv("DB_PORT"), os.Getenv("DB_NAME"))
+	//dsn := "root:password@tcp(localhost:3306)/weapons_and_charas?charset=utf8&parseTime=True&loc=Local"
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{
 		Logger: logger.Default.LogMode(logger.Info),
 	})
