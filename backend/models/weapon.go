8c3be7c0e78428d171dce59f65146dd888785a16
koipoi <koipond575@gmail.com>
2024-05-21 14:09:50 -0400

    Make name a partial search match and add limit to 20 entries with pagination

--- backend/models/weapon.go
+++ backend/models/weapon.go
@@ -1,18 +1,43 @@
 package models
 
+import (
+	"strconv"
+
+	"gorm.io/gorm"
+)
+
 type Weapon struct {
 	// gorm.Model (only used for populating a struct w/ id, createdat, updatedat, deleted at)
-	ID			int				`json:"-" gorm:"primaryKey;"`
-	Name		string			`gorm:"unique"`
-	Element		string	
-	WepType 	string			`json:"WeaponType"`
-	Skills		[]*WeaponSkill	`json:"Skills,omitempty" gorm:"many2many:weapon_skills_relationship;"`
+	ID      int    `json:"-" gorm:"primaryKey;"`
+	Name    string `gorm:"unique"`
+	Element string
+	WepType string         `json:"WeaponType"`
+	Skills  []*WeaponSkill `json:"Skills,omitempty" gorm:"many2many:weapon_skills_relationship;"`
 }
 
 func GetWeaponsByQuery(filters map[string]interface{}) ([]Weapon, error) {
 	var weapons []Weapon
-	// Sanitize the filters in calling function. Throw error if something happens in the DB
-	err := DB.Preload("Skills").Table("Weapons").Where(filters).Find(&weapons).Error
+	var pagination int = 0
+	var err error = nil
+	var dbctx *gorm.DB = DB.Preload("Skills").Table("Weapons")
+
+	// Remove pagination from filters if it exists
+	if filters["pagination"] != nil {
+		pagination, err = strconv.Atoi(filters["pagination"].(string))
+		delete(filters, "pagination")
+	}
+
+	if err != nil {
+		return weapons, err
+	}
+
+	// Do partial match on name
+	if filters["name"] != nil {
+		dbctx = dbctx.Where("name LIKE ?", "%"+filters["name"].(string)+"%")
+		delete(filters, "name")
+	}
+
+	err = dbctx.Where(filters).Limit(20).Offset(20 * pagination).Find(&weapons).Error
 
 	return weapons, err
 }
