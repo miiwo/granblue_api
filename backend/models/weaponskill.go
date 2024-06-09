bcd2baf71712d503831c417e591cfbf509fe2518
koipoi <koipond575@gmail.com>
2024-06-09 00:01:36 -0400

    Add more fields to send in weapon skills: skill level, stat and boost

--- backend/models/weaponskill.go
+++ backend/models/weaponskill.go
@@ -1,7 +1,13 @@
 package models
 
 type WeaponSkill struct {
-	ID			int			`json:"-" gorm:"primaryKey"`
-	Name 		string
-	Description string
+	ID                  int `json:"-" gorm:"primaryKey"`
+	Name                string
+	Description         string
+	BoostType           string
+	StatAffected        string
+	SkillPercLvlOne     string
+	SkillPercLvlTen     string
+	SkillPercLvlFifteen string
+	SkillPercLvlTwenty  string
 }
