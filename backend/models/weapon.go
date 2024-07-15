00b7d886d84d4bb78a152dab8c1152e552a38378
koipoi <koipond575@gmail.com>
2024-07-15 10:45:35 -0400

    Add initial image encoding

--- backend/models/weapon.go
+++ backend/models/weapon.go
@@ -1,6 +1,8 @@
 package models
 
 import (
+	"encoding/base64"
+	"os"
 	"strconv"
 
 	"gorm.io/gorm"
@@ -20,6 +22,8 @@ type Weapon struct {
 	LvlOnefiftyAtk   int
 	LvlTwohundredAtk int
 	Skills           []*WeaponSkill `json:"Skills,omitempty" gorm:"many2many:weapon_skills_relationship;"`
+	Image64          string         `gorm:"-"` // Image field to send to client, does not exist in DB. Base64
+	PictureSmall     string         `json:"-"` // Don't send image link to client, only for internal use
 }
 
 func GetWeaponsByQuery(filters map[string]interface{}) ([]Weapon, error) {
@@ -34,6 +38,7 @@ func GetWeaponsByQuery(filters map[string]interface{}) ([]Weapon, error) {
 		delete(filters, "pagination")
 	}
 
+	// Fail fast
 	if err != nil {
 		return weapons, err
 	}
@@ -44,7 +49,18 @@ func GetWeaponsByQuery(filters map[string]interface{}) ([]Weapon, error) {
 		delete(filters, "name")
 	}
 
+	// DB Query
 	err = dbctx.Where(filters).Limit(20).Offset(20 * pagination).Find(&weapons).Error
+	if err != nil {
+		return weapons, err
+	}
+
+	for _, weapon := range weapons {
+		err = fetchImageInternal(&weapon)
+		if err != nil {
+			return weapons, err
+		}
+	}
 
 	return weapons, err
 }
@@ -54,6 +70,36 @@ func GetWeaponByID(id string) (Weapon, error) {
 	var err error = nil
 
 	err = DB.Preload("Skills").Table("Weapons").Where("name = ?", id).Find(&weapon).Error
+	if err != nil {
+		return weapon, err
+	}
+
+	err = fetchImageInternal(&weapon)
 
 	return weapon, err
+}
+
+func fetchImageInternal(weapon *Weapon) error {
+	var err error = nil
+
+	// Get images based on path
+	//splitImagePath := strings.Split(weapon.PictureSmall, "/")
+	//if len(splitImagePath) == 0 {
+	// I don't have all the images so return without doing anything
+	//	return nil
+	//}
+	//weaponImageName := splitImagePath[len(splitImagePath)-1]
+	//imageFile, err := os.ReadFile(os.Getenv("IMAGE_DIR") + weaponImageName)
+	imageFile, err := os.ReadFile(os.Getenv("IMAGE_DIR") + weapon.PictureSmall)
+	if err != nil {
+		return err
+	}
+
+	// Convert to base64
+	var base64Image string = base64.RawStdEncoding.EncodeToString(imageFile)
+
+	// Attach to returnable result
+	weapon.Image64 = base64Image
+
+	return nil
 }
